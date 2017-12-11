08b5c957abfca70ffa253cacb7188e7d20d25631
kedamaDQ <kedama@foresdon.jp>
2017-12-11 11:51:55 +0900

    Add test for defence_army.(Temporary)

---
+++ src/contents/__tests__/defence_army.js
@@ -0,0 +1,21 @@
+import DefenceArmy from '../defence_army';
+import {
+  MIN,
+  HOUR,
+  DAY
+} from '../../utils/date_utils';
+
+describe('DefenceArmy', () => {
+
+  describe('getMessage', () => {
+    const expected = [
+      expect.stringMatching(/KEY__/)
+    ]
+
+    const da = new DefenceArmy();
+    test('getMessage', () => {
+      expect(da.getMessage().length).toBeGreaterThan(0);
+      expect(da.getMessage()).not.toEqual(expected);
+    })
+  });
+})
