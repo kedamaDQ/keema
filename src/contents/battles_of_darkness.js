bbda026df1aa1eac8306d609278ca55355956ef5
kedamaDQ <kedama@foresdon.jp>
2018-02-12 13:30:10 +0900

    Implemented to function which replies about tomorrow contents.(#31)
    - Cannot reply both about today and tomorrow.
    - Reply priority to the contents of tomorrow than today.

--- src/contents/battles_of_darkness.js
+++ src/contents/battles_of_darkness.js
@@ -3,11 +3,14 @@
 import ContentBase from './content_base';
 import {
   KEY_FULL,
-  KEY_PERIODIC
+  KEY_PERIODIC,
+  WHEN_TOMORROW,
+  WHEN_TODAY
 } from './content_base';
 import {
   HOUR,
-  elapsedDays
+  elapsedDays,
+  tomorrow
 } from '../utils/date_utils';
 
 import Foresdon from '../utils/foresdon_utils';
@@ -30,8 +33,9 @@ export default class BattlesOfDarkness extends ContentBase {
     this.messageProps = this.config().message_props;
   }
 
-  offsetTime(now) {
-    return new Date(now.getTime() - OFFSET_HOURS);
+  offsetTime(now, when) {
+    const d = new Date(now.getTime() - OFFSET_HOURS);
+    return (when === WHEN_TOMORROW) ? tomorrow(d) : d;
   }
 
   getLevel(now, offset) {
@@ -54,21 +58,27 @@ export default class BattlesOfDarkness extends ContentBase {
   }
 
   async getFillings(now, messageProps) {
+    const targetDate = this.offsetTime(now, messageProps.when);
+
     if (messageProps.fillings_key === KEY_PERIODIC || messageProps.fillings_key === KEY_FULL) {
-      const fillings = {};
+      const fillings = {
+        when: this.getWhenString(messageProps.when)
+      };
       const foresdon = new Foresdon();
       for (const enemy of this.enemies) {
-        fillings[`${enemy.key}Level`] = this.getLevel(this.offsetTime(now), enemy.offset);
+        fillings[`${enemy.key}Level`] = this.getLevel(targetDate, enemy.offset);
         fillings[`${enemy.key}Display`] = enemy.display;
         fillings[`${enemy.key}Icon`] = await foresdon.getMonster();
       }
       return fillings;
     } else {
-      const fillings = {};
+      const fillings = {
+        when: this.getWhenString(messageProps.when)
+      };
       this.enemies.find((enemy) => {
         if (enemy.key === messageProps.fillings_key) {
           fillings['name'] = enemy.name;
-          fillings['level'] = this.getLevel(this.offsetTime(now), enemy.offset);
+          fillings['level'] = this.getLevel(targetDate, enemy.offset);
           return true;
         }
         return false;
